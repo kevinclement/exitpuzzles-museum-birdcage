e98fd46b6e7ddf0f4f1ae7b6754ed475c0fbfd0c
Kevin Clement <kevin.clement@gmail.com>
2019-07-17 00:13:09 -0700

    have it checking password

--- museum-birdcage.ino
+++ museum-birdcage.ino
@@ -2,10 +2,13 @@
 #define BUTTON_THRESH 30
 #define BUTTON_DELAY 50
 
+int touch_password[6]             = { 5, 4, 2, 4, 1, 3 };
+int touch_currently_typed[6]      = { 0, 0, 0, 0, 0, 0 };
 int touch_ports[5]                = { 33, 32, 27, 14, 12 };
 unsigned long touch_last_seen[5]  = { 0, 0, 0, 0, 0 };
 bool touch_rising_reported[5]     = { 0, 0, 0, 0, 0 };
 unsigned long touch_first_seen[5] = { 0, 0, 0, 0, 0 };
+int touch_current_pass_index = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -14,18 +17,16 @@ void setup() {
   digitalWrite (LED_PIN, LOW);
 }
 
-void checkButtons() {
+int checkButtons() {
+  int button_pressed = -1;
+  
   // note: looks like 32 and 33 are switched physically.  32 will read from 33 pin, and vice versa
   for(int i=0; i<5; i++) {
-    int v = touchRead(touch_ports[i]);
-    int tls = touch_last_seen[i];
-
-    if (v > BUTTON_THRESH) {
+    if (touchRead(touch_ports[i]) > BUTTON_THRESH) {
 
       // check on rising edge if we've been touching it already
       if (touch_first_seen[i] != 0 && (millis() - touch_first_seen[i] > BUTTON_DELAY)) {
-        Serial.print(i + 1);
-        Serial.println(" failing edge");
+        //Serial.printf("%d falling edge\n", i+1);
       }
 
       // reset last seen
@@ -42,18 +43,49 @@ void checkButtons() {
       if (touch_last_seen[i] != 0) {
         // rising edge
         if (millis() - touch_last_seen[i] > BUTTON_DELAY && !touch_rising_reported[i]) {
-          Serial.print(i + 1);
-          Serial.println(" rising edge");
-          touch_rising_reported[i] = true;
+          //Serial.printf("%d rising edge\n", i+1);          
+          button_pressed = i;
         }
       } else {
         touch_last_seen[i] = millis();
       }
     }
   }
+
+  if (button_pressed >= 0) { 
+    touch_rising_reported[button_pressed] = true;
+  }
+  
+  return button_pressed + 1;
+}
+
+bool checkPassword() {
+  for(int i=0; i<6; i++) {
+    if (touch_currently_typed[i] != touch_password[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void loop() {
-  checkButtons();
-  
+  int buttonPressed = checkButtons();
+  if (buttonPressed != 0) {
+    Serial.printf("%d pressed\n", buttonPressed);
+    touch_currently_typed[touch_current_pass_index] = buttonPressed;
+    
+    if (touch_current_pass_index == 5) {
+      Serial.printf("checking final password...\n");
+      if (checkPassword()) {
+        Serial.printf("SOLVED!!!\n");
+      } else {
+        // now reset the current password stuff
+        touch_current_pass_index = 0;
+        touch_currently_typed[0] = touch_currently_typed[1] = touch_currently_typed[2] = touch_currently_typed[3] = touch_currently_typed[4] = touch_currently_typed[5] = 0;
+      }
+    } else {
+      touch_current_pass_index++;  
+    }
+  }
 }
